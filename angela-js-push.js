0746629c7f9c77b6c0cebf18de5e4ce002e1b280
Greg Bailey <gregbailey617@gmail.com>
2024-01-20 01:06:59 -0500

    kept adding more lessons to the file

--- angela-js-push.js
+++ angela-js-push.js
@@ -1,9 +1,12 @@
-/* var tweet = prompt("enter your tweet here");
+/* var tweet = prompt("enter your tweet here"); */
 
-var tweetLength = tweet.length;
-/* console.log(tweet.length) does not work */
+/* Angela did this as a variable at first. at first i didnt think it the code below would work for some reason but it does
+not sure why it doesn't work in the below example. var tweetLength = tweet.length; */
+/* console.log(tweet.length); */
 
 /* console.log(tweetLength); */
+
+/* alert("you have written " + tweet.length + " characters, and you have " + (140 - tweet.length) + " characters left"); */
 
 /* this was the way that angela was writing and
 then doing the string  */
@@ -16,15 +19,24 @@ submitButton.addEventListener("click", function () {
   var tweetInput = tweet.value;
   var tweetLength = tweetInput.length;
   var tweetOutput = document.querySelector(".output");
-  tweetOutput.innerHTML =
-    "you have entered " +
-    tweetLength +
-    " characters, and you have " +
-    (140 - tweetLength) +
-    " left";
+  tweetOutput.innerHTML = "you have entered " + tweetLength + " characters, and you have " + (140 - tweetLength) + " left";
 });
 /* 
 Angela's 2nd javascript
 
 
 */
+
+/* var secondTweet = prompt("enter your tweet");
+console.log(secondTweet.slice(0, 3)); */
+
+/* Angelas 3rd javascript */
+
+var thirdTweet = prompt("enter your name");
+var firstChar = thirdTweet.slice(0,1);
+var firstCharUpper = firstChar.toUpperCase;
+console.log(firstCharUpper);
+
+
+
+
